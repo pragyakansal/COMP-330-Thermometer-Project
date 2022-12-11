da7473d829bdda93beb06acc160127090329761b
Pragya Kansal <pragyakansal419@gmail.com>
2022-12-11 14:07:06 -0600

    Included code for Sensor class

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,3 +1,22 @@
 package src;
+import java.util.Scanner;
 public class Sensor {
-}
+        // Use a scanner to get input for the next temp reading
+        public static float getTemp() {
+
+            // Create a scanner object.
+            Scanner input = new Scanner(System.in);
+
+            // Display a prompt to enter the temp.
+            System.out.print("Enter your temperature: ");
+
+            // get the temp (double)
+            float temp = input.nextFloat();
+            System.out.println(); // Clear the line
+
+            // return the temp
+            return temp;
+        }
+
+    }
+
